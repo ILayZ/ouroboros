1916cce1b44a27af403088872b7d219e1c9d6878
Ouroboros <ouroboros@users.noreply.github.com>
2026-02-26 13:55:11 +0000

    fix: Strip core.py to ONLY contain ToolBase/ToolEntry definitions (remove broken file_tools import)

--- ouroboros/tools/core.py
+++ ouroboros/tools/core.py
@@ -6,6 +6,3 @@ class ToolEntry:
         self.name = name
         self.schema = schema
         self.function = function
-
-# Core file operations
-from .file_tools import repo_read, repo_list, drive_read, drive_list, drive_write, codebase_digest
